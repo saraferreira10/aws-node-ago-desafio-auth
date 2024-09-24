4fd0683cd97d80ec08bda01cc40361f6f20e1902
Sara Ferreira <sara.f.araujo12@gmail.com>
2024-09-24 10:59:46 -0300

    chore: setup dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({
+  path: path.resolve(__dirname, "..", "src", "config", "config.env"),
+});
 
 const app = express();
 app.use(cors());
-
 app.listen(3000, () => console.log("listening..."));
